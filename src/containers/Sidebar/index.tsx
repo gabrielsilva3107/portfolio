64c6a9c2ecdf12f46f1f3736e32880963b5e06fa
Gabriel Silva <gabrielssilva3107@gmail.com>
2024-10-16 00:49:22 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/gabrielsilva3107.png" />
-    <Titulo fontSize={20}>Gabriel Silva</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Gabriel Silva</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        gabrielsilva3107
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={14}>
+        Desenvolvedor Fullstack
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
